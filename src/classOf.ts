55f132b318c8a73420e40fdfbaa709f86bfe9887
西花 <konglingxing.klx@antgroup.com>
2024-10-22 15:40:39 +0800

    chore(util): 添加注释

--- src/classOf.ts
+++ src/classOf.ts
@@ -1,6 +1,7 @@
 import Types from './types';
 import { getClassName, ANONYMOUS } from './utils';
 
+// 获取值的类型
 const classOf = (o) => {
     if (o === null) { return 'null'; }
     if (o === undefined) { return 'undefined'; }
